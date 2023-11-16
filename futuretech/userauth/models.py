318c08b5541c4dbea08d011981bbbd2d6fd11256
juanpahv <dcrunneryt1@gmail.com>
2023-11-16 15:58:18 -0600

    userauth models

--- futuretech/userauth/models.py
+++ futuretech/userauth/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    id = models.AutoField(primary_key=True)
+    userId = models.ForeignKey(User)
+    user = models.OneToOneField(User)
+    creationDate = models.DateTimeField(auto_now_add=True)
+    profilePicture = models.BinaryField(null=True)
+

38e99230f57f373cff253527be3adc29dac63656
juanpahv <dcrunneryt1@gmail.com>
2023-11-17 15:39:16 -0600

    all models completed

---
+++ futuretech/sales/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+from futuretech.posts.models import Post
+
+class Sale(models.Model):
+  userId = models.ForeignKey(User, on_delete=models.CASCADE)
+  address = models.CharField(max_length=200)
+  isPaid = models.BooleanField(default=False)
+
+class ProductSold(models.Model):
+  postId = models.ForeignKey(Post, on_delete=models.CASCADE)
+  saleId = models.ForeignKey(Sale, on_delete=models.CASCADE)
+  quantity = models.IntegerField(default=0)
+  pricePerunit = models.DecimalField(max_digits=8, decimal_places=2)
+  amount = models.IntegerField(default=1)
